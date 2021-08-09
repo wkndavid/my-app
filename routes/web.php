d489cb73ef56a71d0689c4c1806cef046e7853f2
wkndavid <workingnown@gmail.com>
2021-08-08 21:30:10 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::redirect('rota-a','rota-b', 301); 
-//Route::permanentRedirect('rota-b', 'rota-b');
+//Route::redirect('rota-a', 'rota-b', 301);
+//Route::permanentRedirect('rota-a', 'rota-b');
 
-//Route::get('rota-a', function(){
-//Lógica cabulosa
-//return redirect()->route('rotab');
-//
-
-//Route::get('rota-b', function(){
-//return 'ROTA B';
-//name('rotab');
-
-Route::view('/welcome', 'welcome' => );
-
-Route::get('/', function(){
-    return view('welcome');
+Route::get('rota-a', function(){
+    // lógica cabulosa
+    return redirect()->route('rotab');
 });
+
+Route::get('rota-b', function(){
+    return 'ROTA B';
+})->name('rotab');
+
+
+Route::get('users', function(){
+    return 'hello world';
+})->name('users');
