4b390c1ff32d2d45154f9c6361dca689bd87bc7b
wkndavid <workingnown@gmail.com>
2021-08-09 11:35:15 -0300

    rotas-de-vizualizacao-laravel

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,26 @@ use Illuminate\Support\Facades\Route;
 //Route::redirect('rota-a', 'rota-b', 301);
 //Route::permanentRedirect('rota-a', 'rota-b');
 
-Route::get('rota-a', function(){
+//Route::get('rota-a', function(){
     // lógica cabulosa
-    return redirect()->route('rotab');
+    //return redirect()->route('rotab');
+//});
+
+//Route::get('rota-b', function(){
+    //return 'ROTA B';
+//})->name('rotab');
+Route::get('token/{token}', function($token){
+    return $token;
+})->whereUuid('token');
+
+
+Route::get('/user/{id}/{name}', function($id =null, $name=null){
+    return 'User '. $id. '-' .$name;
+})->where([
+    'id'=>'[0-9]+',
+    'name'=>'[A-Za-z]+'
+]);
+
+Route::get('/', function(){
+    return view('welcome');
 });
-
-Route::get('rota-b', function(){
-    return 'ROTA B';
-})->name('rotab');
-
-
-Route::get('users', function(){
-    return 'hello world';
-})->name('users');
