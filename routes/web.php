5b874296743ac30d2dcd9adc4fb1748e69ace9fc
wkndavid <workingnown@gmail.com>
2021-08-13 11:21:18 -0300

    Fallback

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::redirect('rota-a', 'rota-b', 301);
-//Route::permanentRedirect('rota-a', 'rota-b');
+Route::get('test', function(){
+        return 'test';
+       })->middleware('signed');
 
-//Route::get('rota-a', function(){
-    // lógica cabulosa
-    //return redirect()->route('rotab');
-//});
+Route::middleware('signed')->group(function(){
+    Route::get('user', function(){
+        return 'Hello World';
+    })->name('users');
+    Route::get('user/{id}', function($id){
+        return 'Hello World' . $id;
+    })->name('user');
+});
 
-//Route::get('rota-b', function(){
-    //return 'ROTA B';
-//})->name('rotab');
-
-Route::domain('{user}.cursolaravelpro.test')->group(function(){
-    Route::get('{id}', function($user, $id){
-        return $user. '-' . $id;
-       });
+Route::fallback(function(){
+    return view('welcome');
 });
