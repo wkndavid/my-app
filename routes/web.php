fd854364083fc42193cd7f78329e1af00dcc97f7
wkndavid <workingnown@gmail.com>
2021-08-13 22:20:31 -0300

    Injecao-de-depedencias

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function(){
-        return 'test';
-       })->middleware('signed');
 
-Route::middleware('signed')->group(function(){
-    Route::get('user', function(){
-        return 'Hello World';
-    })->name('users');
-    Route::get('user/{id}', function($id){
-        return 'Hello World' . $id;
-    })->name('user');
-});
-
-Route::fallback(function(){
-    return view('welcome');
+Route::get('/', function(Illuminate\Http\Request $request){
+    dump($request);
+    //dd($request);
+    //return $request;        
 });
