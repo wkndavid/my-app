ab61f4c90c805de514c5a6890a23c236c1847bb4
wkndavid <workingnown@gmail.com>
2021-09-10 19:59:31 -0300

    Controllers: Resource controller - criando resources

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\UserController;
 
-//Route::redirect('rota-a','rota-b', 301); 
-//Route::permanentRedirect('rota-b', 'rota-b');
-
-//Route::get('rota-a', function(){
-//Lógica cabulosa
-//return redirect()->route('rotab');
-//
-
-//Route::get('rota-b', function(){
-//return 'ROTA B';
-//name('rotab');
-
-//Route::get('users', function (){
-//return 'OLA MUNDO!';
-//name('users');
-
-
-Route::get('/', function(){
-    return view('welcome');
-});
+Route::resource('users', UserController::class);
