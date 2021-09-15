50406ccfcf39c818d50712d0463a6d1855cf359c
wkndavid <workingnown@gmail.com>
2021-09-14 21:27:50 -0300

    Controllers: Resource Controllers - adicionando rotas extras

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 //Route::get('users/{user:email}', [UserController::class, 'show']);
+Route::get('usuario/posts', [UserController::class, 'posts'])->name('usuarios.posts');
 
 Route::resource('users', UserController::class)->scoped([
     'user' => 'email',
