96d21cc64ded638fa067586949e0cb3728c628d0
wkndavid <workingnown@gmail.com>
2021-09-15 20:45:11 -0300

    Request: acessando dados da url

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 //Route::get('users/{user:email}', [UserController::class, 'show']);
-Route::get('usuario/posts', [UserController::class, 'posts'])->name('usuarios.posts');
+Route::get('user/{user}', function(Illuminate\Http\Request $request){
+    //dd($request);
 
-Route::resource('users', UserController::class)->scoped([
-    'user' => 'email',
-]);
+    // dd($request->path());
+    // dd($request->url());
+    // dd($request->fullUrl());
+    // dd($request->fullUrlWithQuery(['curso' => 'Laravel']));
+    // dd($request->fullUrlIs('http://127.0.0.1:8000/user?token=abc'));
+    // dd($request->is('user/*'));
+    // dd($request->routeIs('user'));
+    // dd($request->method() === 'GET')
+    dd($request->isMethod('get'));
+})->name('user');
