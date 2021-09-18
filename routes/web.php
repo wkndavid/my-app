7690be900557045fc131cba3ea2e56e30885d815
wkndavid <workingnown@gmail.com>
2021-09-17 21:56:14 -0300

    Views: criando a primeira view

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
-Route::get('user/{user}', function(Illuminate\Http\Request $request){
-    //dd($request->input('token'));
-    //dd($request->input('curso', 'Laravel'));
-    //dd($request->input('product.2.versao'));
-    //dd($request->input());
-    //dd($request->query());
-    //dd($request->query('curso', 'laravel'));
-    //dd($request->token);
-    //dd($request->only('product', 'token'));
-    //dd($request->only('token'));
-    dd($request->except('product'));
-})->name('user');
+Route::get('/', function(){
+    // return View::make('welcome');
+   return view('welcome');
+});
+
+Route::get('/profile', function(){  
+    dd(View::exists('user.profile'));
+    //return View::first(['user.profile', 'user.profile_new']);
+    return view('user.profile');
+});
