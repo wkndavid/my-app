d2ef4e4ae0835cb35ba86db15e93afc2524c8706
wkndavid <workingnown@gmail.com>
2021-09-21 21:27:26 -0300

    BLADE TEMPLATE: Diretivas para if/else

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index(){
 
-       $users = User::all();
+       $users = [];
        
         return view('user.index' , compact('users'));
     }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,13 +8,32 @@
 </head>
 <body>
     <h1>Usuários</h1>
-{{  dd($users) }}
-<?php 
-   foreach ($users as $user)
-   {
-      // echo $user;
-   }
-?>
+    {{ count($users) }} <br>
+
+    @if(count($users) === 1)
+        Eu tenho 1 usuários
+    @elseif(count($users) > 1)
+        Eu tenho vários usuários
+    @else
+        Eu não tenho usuários
+    @endif
+
+    <br><br>
+    @unless(count($users))
+        Eu não tenho Usuários
+    @endunless
+
+    <br><br>
+
+    @isset($users)
+        Variável users existe
+    @endisset
+
+    <br><br>
+    @empty($users)
+        Variável users está vazia
+    @endempty
+
 <br>
 </body>
 </html>
