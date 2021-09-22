b73730c7e53d8265a4e23c737347c34d3a9c9bce
wkndavid <workingnown@gmail.com>
2021-09-21 21:59:27 -0300

    BLADE TEMPLATE: Parando ou continuando o foreach

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,29 +8,19 @@
 </head>
 <body>
     <h1>Usuários</h1>
-
     {{ count($users) }} <br>
 
-    @for($i = 0; $i <= count($users); $i++)
-        {{ $i }}
-    @endfor   
-    <br><br>
-
     @foreach($users as $user)
-        {{$user->name}} ({{$user->email}})<br>   
+        @if($user->id === 4)
+            @continue
+        @endif
+
+        {{$user->id}} - {{$user->name}} ({{$user->email}})<br>   
+
+        @if($user->id === 5)
+            @break
+        @endif
     @endforeach
     <br><br>
-
-    @forelse($users as $user)
-        {{$user->name}} ({{$user->email}}) <br>
-    @empty
-        Nenhum usuário encontrado
-    @endforelse
-<br><br>
-<?php $j = 1; ?>
-@while($j <= 10)
-{{$j++}}
-@endwhile
-<br>
 </body>
 </html>
