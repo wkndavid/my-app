589614cd9238299af170dc60d9723edfdfad2f4b
wkndavid <workingnown@gmail.com>
2021-09-24 20:24:07 -0300

    BLADE LAYOUTS: Criando layouts usando herança de template

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
-</head>
-<body>
-    @php
-        $count = count($users);
-        
-        foreach($users as $user){
-            echo $user->name . '<br>';
-      }
-        
-    @endphp
+@extends('layouts.default')
 
-    @include('heading', [
-        'title' => 'Usuários'
-    ])
-
+@section('content')
     @each('user.user', $users, 'user')
-</body>
-</html>
+@endsection
+
+
+{{--    @php --}}
+{{--        $count = count($users); --}}
+        
+{{--        foreach($users as $user){ --}}
+{{--            echo $user->name . '<br>'; --}}
+{{--      }
+{{--    @endphp --}}
+
+{{--    @include('heading', --}}
+{{--         'title' => 'Usuários'--}}
+{{--     ]) --}}
+{{--     @each('user.user', $users, 'user')--}}
