c14c34e2a20dc4843cc925779be7dc6fe7493255
wkndavid <workingnown@gmail.com>
2021-09-25 22:14:32 -0300

    BLADE STACKS: Criando Stacks

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @stack('css')
     <title>@yield('title', 'Laravel App')</title>
 </head>
 <body>
@@ -15,7 +16,7 @@
       </ul>
     @show
     <br><br>
-    @yield('content')
-
+    @yield('content')<br>
+    @stack('scripts')
 </body>
 </html>
