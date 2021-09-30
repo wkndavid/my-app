18869155a42dec32820a970f7ef353d432063dce
wkndavid <workingnown@gmail.com>
2021-09-29 21:54:06 -0300

    BLADE COMPONENTES: Diretivas

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel App</title>
+</head>
+<body>
+    @section('content')
+    @show
+
+    @yield('yield', 'Yield')
+</body>
+</html>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
-</head>
-<body>
-    
-    @include('heading', [
-        'title' => 'Usuários'
-    ])
+@extends('layouts.default')
 
-    @each('user.user', $users, 'user')    
+@section('title', 'Lista de usuarios')
 
-{{--    @foreach($users as $user)   --}}
-{{--    @include('user.user')   --}}
-{{--    @endforeach --}}
-</body>
-</html>
+@section('content')
+    @each('user.user', $users, 'user')
+@endsection
+
+@section('yield')
+    Conteúdo yield
+@endsection
